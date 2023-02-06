1ab50718177954b457922bc72f65cab798af261b
JUNTAO LIU <106684251+juntaodev@users.noreply.github.com>
2023-02-06 11:15:21 -0500

    add nav to home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { ThemeProvider } from "./context/ThemeContext";
 
 function App() {
   return (
-    <div>
-    
-    </div>
+    <ThemeProvider>
+      
+        <Navbar />
+      
+    </ThemeProvider>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { Link, useNavigate } from "react-router-dom";
+import ThemeToggle from "./ThemeToggle";
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className="rounded-div flex items-center justify-between h-20 font-bold">
+      <Link to="/">
+        <h1 className="text-2xl ">Screener</h1>
+      </Link>
+      <div className="hidden md:block">
+        <ThemeToggle />
+      </div>
     </div>
   )
 }
