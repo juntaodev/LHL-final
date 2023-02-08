5b777ded57e6477c89257883a6f9d374a383c5ee
JUNTAO LIU <106684251+juntaodev@users.noreply.github.com>
2023-02-08 11:43:40 -0500

    route stockdetail page

--- src/routes/StockDetail.jsx
+++ src/routes/StockDetail.jsx
@@ -1,20 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from "react-router-dom";
+import StockOverview from '../components/StockOverview';
+import StockHeader from '../components/StockHeader';
 
 function StockDetail() {
   const [data, setData] = useState({});
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const params = useParams();
+  const param = useParams();
 
   useEffect(() => {
     setLoading(true);
     const options = {
       method: 'GET',
       url: 'https://twelve-data1.p.rapidapi.com/logo',
-      params: {symbol: `${params.stockSymbol}`},
+      params: {symbol: `${param.stockSymbol}`},
       headers: {
         'X-RapidAPI-Key': '3739bf6e15msh3f361324e7ae496p1291a4jsneeac436b4fc4',
         'X-RapidAPI-Host': 'twelve-data1.p.rapidapi.com'
@@ -31,15 +33,24 @@ function StockDetail() {
         setError(error);
         setLoading(false);
       });
-  }, []);
+  }, [param.stockSymbol]);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>An error occurred: {error.message}</p>;
 
   return (
     <div className="rounded-div h-[1080px]">
+      <div>
+        <StockHeader />
+      </div>
+      <div className="flex">
+        
+        <StockOverview />
+      </div>
       <h2>Company LOGO</h2>
-      <img src={data} alt="company logo"/>
+      <img className="mask mask-circle"  src={data} alt="company logo"/>
+      
+
       
     </div>
   );
