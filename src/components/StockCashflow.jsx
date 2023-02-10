3983a2c739bf50df8a2f5836f708b155d687d507
JUNTAO LIU <106684251+juntaodev@users.noreply.github.com>
2023-02-10 11:55:01 -0500

    update style table for cashflow

--- src/components/StockCashflow.jsx
+++ src/components/StockCashflow.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from "react-router-dom";
+import { NumericFormat } from 'react-number-format';
 
 const StockCashflow = () => {
 
@@ -20,7 +21,7 @@ const StockCashflow = () => {
         const response = await axios.get(url);
         setStockCashflow(response.data);
         setLoading(false);
-        console.log("cashflow", response.data);
+        // console.log("cashflow", response.data);
       } catch (error) {
         setError(error);
       }
@@ -29,6 +30,15 @@ const StockCashflow = () => {
     fetchData();
   }, [url]);
 
+  const yearList = stockCashflow?.annualReports.map((annualReport) => {
+    
+    return annualReport
+  })
+
+  const showInThousands = (input) => {
+    return Number(input) / 1000
+  }
+
   if (!stockCashflow) {
     return <div>Loading...</div>;
   }
@@ -36,12 +46,84 @@ const StockCashflow = () => {
 
   return (
     <div>
-
       {/* need a 6 columns table, first column is key from API, other 5 columns are value from last 5 years*/}
-
-      {/* needed key from top to bottom : totalAssets, totalCurrentAssets, cashAndCashEquivalentsAtCarryingValue, totalNonCurrentAssets, intangibleAssets, goodwill, totalLiabilities , totalCurrentLiabilities, currentDebt, currentAccountsPayable, totalNonCurrentLiabilities, longTermDebt, totalShareholderEquity, commonStock, commonStockSharesOutstanding  */}
-
+      {/* needed key from top to bottom : operatingCashflow,   */}
       {/* */}
+
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          <thead>
+          <tr>
+            <th className="lowercase text-primary">* number shown in thousands</th>
+            {yearList.map((element) => {
+              return <th key={element.fiscalDateEnding} className="text-lg text-primary">{element.fiscalDateEnding.slice(0,4)}</th>
+            })}
+          </tr>
+          </thead>
+          <tbody>
+          <tr className="hover text-md text-secondary">
+            <th className='font-normal'>Cash from Operating Activities</th>
+            {yearList.map((element) => {
+              return <th key={element.fiscalDateEnding} className='font-normal'><NumericFormat value={showInThousands(element.operatingCashflow)} thousandSeparator="," displayType="text"/></th>
+            })}
+          </tr>
+          <tr className="hover text-md text-secondary">
+            <th className='font-normal'>Net Income</th>
+            {yearList.map((element) => {
+              return <th key={element.fiscalDateEnding} className='font-normal'><NumericFormat value={showInThousands(element.netIncome)} thousandSeparator="," displayType="text"/></th>
+            })}
+          </tr> 
+          <tr className="hover text-md text-secondary">
+            <th className='font-normal'>Depreciation&Amortization</th>
+            {yearList.map((element) => {
+              return <th key={element.fiscalDateEnding} className='font-normal'><NumericFormat value={showInThousands(element.depreciationDepletionAndAmortization)} thousandSeparator="," displayType="text"/></th>
+            })}
+          </tr>
+          <tr className="hover text-md text-secondary">
+            <th className='font-normal'>Cashf from Investing Activities</th>
+            {yearList.map((element) => {
+              return <th key={element.fiscalDateEnding} className='font-normal'><NumericFormat value={showInThousands(element.cashflowFromInvestment)} thousandSeparator="," displayType="text"/></th>
+            })}
+          </tr>
+          <tr className="hover text-md text-secondary">
+            <th className='font-normal'>Cashflow From Financing Activities</th>
+            {yearList.map((element) => {
+              return <th key={element.fiscalDateEnding} className='font-normal'><NumericFormat value={showInThousands(element.cashflowFromFinancing)} thousandSeparator="," displayType="text"/></th>
+            })}
+          </tr>
+          <tr className="hover text-md text-secondary">
+            <th className='font-normal'>Debt repayment</th>
+            {yearList.map((element) => {
+              return <th key={element.fiscalDateEnding} className='font-normal'><NumericFormat value={showInThousands(element.proceedsFromRepaymentsOfShortTermDebt)} thousandSeparator="," displayType="text"/></th>
+            })}
+          </tr>
+          <tr className="hover text-md text-secondary">
+            <th className='font-normal'>Dividend Payout</th>
+            {yearList.map((element) => {
+              return <th key={element.fiscalDateEnding} className='font-normal'>{element.dividendPayout === "None"? "--" : <NumericFormat value={showInThousands(element.dividendPayout)} thousandSeparator="," displayType="text"/>}</th>
+            })}
+          </tr>
+          <tr className="hover text-md text-secondary">
+            <th className='font-normal'>Change In Cash And CashEquivalents</th>
+            {yearList.map((element) => {
+              return <th key={element.fiscalDateEnding} className='font-normal'><NumericFormat value={showInThousands(element.changeInCashAndCashEquivalents)} thousandSeparator="," displayType="text"/></th>
+            })}
+          </tr>
+          <tr className="hover text-md text-secondary">
+            <th className='font-normal'>Capital Expenditures</th>
+            {yearList.map((element) => {
+              return <th key={element.fiscalDateEnding} className='font-normal'><NumericFormat value={showInThousands(element.capitalExpenditures)} thousandSeparator="," displayType="text"/></th>
+            })}
+          </tr>
+          <tr className="hover text-md text-secondary">
+            <th className='font-normal'>Free Cashflow</th>
+            {yearList.map((element) => {
+              return <th key={element.fiscalDateEnding} className='font-normal'><NumericFormat value={showInThousands(element.operatingCashflow - element.capitalExpenditures)} thousandSeparator="," displayType="text"/></th>
+            })}
+          </tr>
+          </tbody>
+        </table>
+      </div>
 
     </div>
   )
