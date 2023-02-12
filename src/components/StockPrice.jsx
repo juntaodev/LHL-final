92ca784d77542d94b76a3da0a908c4ecec18419f
JUNTAO LIU <106684251+juntaodev@users.noreply.github.com>
2023-02-11 22:37:14 -0500

    add loading and alert to stock header components

--- src/components/StockPrice.jsx
+++ src/components/StockPrice.jsx
@@ -35,16 +35,27 @@ function StockPrice() {
   }, [param.stockSymbol]);
 
   if (!stockPrice) {
-    return <div>Loading...</div>;
+     return <progress className="progress w-56"></progress>;
   }
-  if (error) return <p>An error occurred: {error.message}</p>;
+  if (loading) {
+    return <progress className="progress w-56"></progress>;
+  }
+  if (error) {
+    return (
+      <div className="alert alert-error shadow-lg">
+        <div>
+          <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+          <span>Error! Something is Wrong! Try Refresh!</span>
+        </div>
+      </div>)
+  }
   
   return (
 
     <div className="indicator">
       <span className="indicator-item indicator-bottom badge badge-sm ">USD</span>
       <div className="py-2 card bg-base-200">
-        <p className='text-2xl font-bold text-gray-700 px-2'>${Number(stockPrice).toFixed(2)}</p>
+        <p className='text-2xl font-bold text-secondary px-2'>${Number(stockPrice).toFixed(2)}</p>
       </div>
     </div>
   )
--- src/components/StockTitle.jsx
+++ src/components/StockTitle.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from "react-router-dom";
-
+import {Link} from "react-router-dom";
 
 function StockTitle() {
   const [stock, setstock] = useState(null);
@@ -30,25 +30,38 @@ function StockTitle() {
   }, [url]);
 
   if (!stock) {
-    return <div>Loading...</div>;
+    return <progress className="progress w-56"></progress>;
   }
-  if (error) return <p>An error occurred: {error.message}</p>;
+  if (loading) {
+    return <progress className="progress w-56"></progress>;
+  }
+  if (error) {
+    return (
+      <div className="alert alert-error shadow-lg">
+        <div>
+          <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+          <span>Error! Something is Wrong! Try Refresh!</span>
+        </div>
+      </div>)
+  }
   
   return (
     <div className="py-2">
 
-        <div className='text-center'>
-      
-          <span className='py-2 text-2xl font-bold  text-gray-700'>{stock?.Name}</span> 
+        <div className='text-center '>
+          <Link to={`/stock/${param.stockSymbol}`}>
+          <span className='p-2 text-2xl font-bold text-secondary hover:bg-base-200 card'>{stock?.Name}</span>
+          </Link >      
+           
         </div>
 
         {/* The button to open modal */}
-        <label htmlFor="symbol" className="btn btn-block btn-ghost text-lg text-gray-600">{stock?.Symbol} </label>
+        <label htmlFor="symbol" className="btn btn-block btn-ghost text-lg text-secondary">{stock?.Symbol} </label>
 
         {/* Put this part before </body> tag */}
         <input type="checkbox" id="symbol" className="modal-toggle" />
         <label htmlFor="symbol" className="modal cursor-pointer">
-          <label className="modal-box relative text-gray-600" htmlFor="">
+          <label className="modal-box relative text-secondary" htmlFor="">
             
             <p className="py-4">This is the ticker symbol of {stock?.Name}. Ticker symbols are used on stock exchanges because they are easy abbreviations that are useful to investors and analysts to help them identify stocks and obtain all relevant information such as stock split, dividend data, earnings reports, etc. There are some companies that trade with two different symbols on the same stock market because they offer two classes of shares, one with voting rights and another without voting rights.</p>
             <p className="py-4">An example is Alphabet, formerly known as Google, which trades under the symbols GOOGL and GOOG, representing the company's class A shares and class C shares, respectively. The symbols also help in identifying the type of shares traded on different stock exchanges. They are presented in different patterns in different indices, which makes them easy to find on trading sites and stock exchange websites.</p>
