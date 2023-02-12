7e004931a9bf288c4b948997c0aa0f82857a61a9
JUNTAO LIU <106684251+juntaodev@users.noreply.github.com>
2023-02-11 22:22:20 -0500

    add loading and alert to evaluation components

--- src/components/CompanyGrowth.jsx
+++ src/components/CompanyGrowth.jsx
@@ -52,12 +52,23 @@ const CompanyGrowth = () => {
   };
 
   if (!stockIncome) {
-    return <div>Loading...</div>;
+    return <progress className="progress w-56"></progress>;
   }
   if (!stockCash) {
-    return <div>Loading...</div>;
+    return <progress className="progress w-56"></progress>;
   }
-  if (error) return <p>An error occurred: {error.message}</p>;
+  if (loading) {
+    return <progress className="progress w-56"></progress>;
+  }
+  if (error) {
+    return (
+      <div className="alert alert-error shadow-lg">
+        <div>
+         <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+         <span>Error! Something is Wrong! Try Refresh!</span>
+        </div>
+      </div>)
+  }
 
   //key variables 
   const latestFiscalYear = stockIncome?.annualReports[0]?.fiscalDateEnding.slice(0,4);
--- src/components/CompanyProfit.jsx
+++ src/components/CompanyProfit.jsx
@@ -52,12 +52,23 @@ const CompanyProfit = () => {
   };
 
   if (!stockIncome) {
-    return <div>Loading...</div>;
+    return <progress className="progress w-56"></progress>;
   }
   if (!stockCash) {
-    return <div>Loading...</div>;
+    return <progress className="progress w-56"></progress>;
   }
-  if (error) return <p>An error occurred: {error.message}</p>;
+  if (loading) {
+    return <progress className="progress w-56"></progress>;
+  }
+  if (error) {
+    return (
+      <div className="alert alert-error shadow-lg">
+        <div>
+         <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+         <span>Error! Something is Wrong! Try Refresh!</span>
+        </div>
+      </div>)
+  }
   
   // key variables
   const latestFiscalYear = stockIncome?.annualReports[0]?.fiscalDateEnding.slice(0,4);
@@ -88,7 +99,7 @@ const CompanyProfit = () => {
             <td className='p-4 bg-indigo-50'>Gross Margin</td>
             <td className='p-4 text-right'>{decToPercentage(grossMargin)}</td>
             
-            <td className='p-4 text-right'>{grossMargin > 0.5 ? <FlagFilled style={{color:"#15803d"}}/> : <FlagFilled style={{color:"#dc2626"}}/>}</td>
+            <td className='p-4 text-right'>{grossMargin > 0.4 ? <FlagFilled style={{color:"#15803d"}}/> : <FlagFilled style={{color:"#dc2626"}}/>}</td>
           </tr>
           <tr className="hover text-base text-secondary">
             <td className='p-4 bg-indigo-50'>Operating Margin</td>
