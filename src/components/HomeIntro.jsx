bfd0f66c5c9db991a9ac930ab14a719cf73d9f65
JUNTAO LIU <106684251+juntaodev@users.noreply.github.com>
2023-02-12 23:52:57 -0500

    update home page

---
+++ src/components/HomeIntro.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const HomeIntro = () => {
+  return (
+    <div className="hero min-h-50% pb-4 ">
+      <div className="hero-content text-center bg-gradient-to-r from-red-400 to-green-400 rounded-2xl ">
+        <div className="max-w-md">
+          <h1 className="text-4xl font-bold text-neutral-50">Welcome to Stock 101</h1>
+          <p className="py-6 text-neutral-50 ">If you are an amature investor in stock market, this is a perfect website to study stocks and analyze companies. </p>
+          
+        </div>
+      </div>
+    </div>
+
+  )
+}
+
+export default HomeIntro
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,19 +1,22 @@
 import React from "react";
-import About from "../components/About";
+import HomeIntro from "../components/HomeIntro";
 import Accordions from "../components/Accordions";
 import StockList from "../components/StockList";
-import StockSearch from "../components/StockSearch";
+import AutoCompleteSearch from "../components/AutoCompleteSearch";
+import TrendingStocks from "../components/TrendingStocks";
 
 const Home = () => {
   return (
-    <div>
-      <div>
-      <About />
-      <Accordions />
+    <div className="max-w-[1280px] w-full mx-auto p-8 min-h-screen">
+      <div className="">
+
+        <HomeIntro />
+        <AutoCompleteSearch />
+      
       </div>
-      <div>
-      <StockSearch />
-      <StockList />
+      <div className="">
+      
+        <TrendingStocks />
       </div>
     </div>
   );
